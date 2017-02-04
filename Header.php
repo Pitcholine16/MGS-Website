6cfa651932421c64a39c962ace93510445ba92b9
PC-KILLIAN_DV\killian <monarch.games.studio.direct@gmail.com>
2017-02-04 12:22:23 +0100

    Modification des chemins --> Ok

--- Header.php
+++ Header.php
@@ -4,50 +4,50 @@
 <head>
     <meta charset="utf-8" />
     <title>MGS - Accueil</title>
-    <link rel="stylesheet" href="/Site Web/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/Site Web/styleHeader.css"/>
+    <link rel="stylesheet" href="/site_mgs/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/site_mgs/styleHeader.css"/>
 </head>
 
 <header>
     <aside class="container-fluid menu">
         <div class=" wrap">
-            <img id="logo" src="/Site Web/images/MGS_logo_thumb.png" alt="Monarch Games Studio">
+            <img id="logo" src="/site_mgs/images/MGS_logo_thumb.png" alt="Monarch Games Studio">
         </div>
             <nav id="menu">
                 <ul id="bar">
                     <li class="nav-item">
-                        <a class="a-nav-item" href="/Site Web/home.php">Accueil</a>
+                        <a class="a-nav-item" href="/site_mgs/home.php">Accueil</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="a-nav-item" href="/Site Web/play.php">Jouer</a>
+                        <a class="a-nav-item" href="/site_mgs/play.php">Jouer</a>
                         <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/play/pc.php">PC</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/play/android.php">Android</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/play/ios.php">iOS</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/pc.php">PC</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/android.php">Android</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/ios.php">iOS</a></li>
                         </ul>
                     </li>
 
                     <li class="nav-item">
-                        <a class="a-nav-item" href="/Site Web/project.php">Le Projet</a>
+                        <a class="a-nav-item" href="/site_mgs/project.php">Le Projet</a>
                         <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/project/partners.php">Les partenaires</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/project/rnja.php">Le R.N.J.A.</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/partners.php">Les partenaires</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/rnja.php">Le R.N.J.A.</a></li>
                         </ul>
                     </li>
 
                     <li class="nav-item">
-                        <a class="a-nav-item" href="/Site Web/news.php">Les News</a>
+                        <a class="a-nav-item" href="/site_mgs/news.php">Les News</a>
                         <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/news/games.php">Nos nouveaux jeux</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/news/games.php">Nos nouveaux jeux</a></li>
                         </ul>
                     </li>
 
                     <li class="nav-item">
-                        <a class="a-nav-item" href="/Site Web/contact.php">Contactez-nous</a>
+                        <a class="a-nav-item" href="/site_mgs/contact.php">Contactez-nous</a>
                         <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/contact/join.php">Rejoignez-nous</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/Site Web/contact/contactus.php">Contactez-nous</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/join.php">Rejoignez-nous</a></li>
+                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/contactus.php">Contactez-nous</a></li>
                         </ul>
                     </li>
 
