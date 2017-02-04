e87c355d0008e4a9766d90ecf72a053993ff389f
PC-KILLIAN_DV\killian <monarch.games.studio.direct@gmail.com>
2017-02-04 14:59:47 +0100

    Test

--- Header.php
+++ Header.php
@@ -1,8 +1,9 @@
-
+<!-- lol -->
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MGS - Accueil</title>
     <link rel="stylesheet" href="/site_mgs/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/site_mgs/styleHeader.css"/>
@@ -13,46 +14,41 @@
         <div class=" wrap">
             <img id="logo" src="/site_mgs/images/MGS_logo_thumb.png" alt="Monarch Games Studio">
         </div>
-            <nav id="menu">
-                <ul id="bar">
-                    <li class="nav-item">
-                        <a class="a-nav-item" href="/site_mgs/home.php">Accueil</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="a-nav-item" href="/site_mgs/play.php">Jouer</a>
-                        <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/pc.php">PC</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/android.php">Android</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/ios.php">iOS</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="a-nav-item" href="/site_mgs/project.php">Le Projet</a>
-                        <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/partners.php">Les partenaires</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/rnja.php">Le R.N.J.A.</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="a-nav-item" href="/site_mgs/news.php">Les News</a>
-                        <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/news/games.php">Nos nouveaux jeux</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="a-nav-item" href="/site_mgs/contact.php">Contactez-nous</a>
-                        <ul class="sub-nav">
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/join.php">Rejoignez-nous</a></li>
-                            <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/contactus.php">Contactez-nous</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </nav>
+        <nav id="menu">
+            <ul id="bar">
+                <li class="nav-item">
+                    <a class="a-nav-item" href="/site_mgs/home.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="a-nav-item" href="/site_mgs/play.php">Jouer</a>
+                    <ul class="sub-nav">
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/pc.php">PC</a></li>
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/android.php">Android</a></li>
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/play/ios.php">iOS</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="a-nav-item" href="/site_mgs/project.php">Le Projet</a>
+                    <ul class="sub-nav">
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/partners.php">Les partenaires</a></li>
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/project/rnja.php">Le R.N.J.A.</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="a-nav-item" href="/site_mgs/news.php">Les News</a>
+                    <ul class="sub-nav">
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/news/games.php">Nos nouveaux jeux</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="a-nav-item" href="/site_mgs/contact.php">Contactez-nous</a>
+                    <ul class="sub-nav">
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/join.php">Rejoignez-nous</a></li>
+                        <li class="sub-nav-item"><a class="asub-nav-item" href="/site_mgs/contact/contactus.php">Contactez-nous</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
 
     </aside>
 </header> 
