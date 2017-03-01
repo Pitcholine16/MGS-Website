2f3726e6da772b09927639261e87a13f5422566b
Olivier Courmont <nano.micro.game.gestionnaire@gmail.com>
2017-03-01 17:28:16 +0100

    Mise en forme de la page avec les nouveau <section> (Seulement pour 1280+px)

--- home.php
+++ home.php
@@ -11,50 +11,66 @@
     
 <div class="main_wrapper">
     
-    <h1 title="Des BG!">Qui sommes nous?</h1>
-
-    <img src="images/sigle_com.png" alt="Sigle Communauté" />
-
-    <img src="images/sigle_geek.png" alt="Sigle Mannette" />
-
-    <img src="images/sigle_artiste.png" alt="Sigle Crayon" />
-
-    <img src="images/sigle_echange.png" alt="Sigle Echange" />
-
-    <h2>Une communauté</h2>
+<h1>Qui sommes nous?</h1>
     
-    <h2>Des gamers!</h2>
+    <section>
+
+        <img src="images/sigle_com.png" alt="Sigle Communauté" />
+
+        <h2>Une communauté</h2>
+
+        <p>Nous sommes avant tout, une communauté centrée sur l’univers des jeux vidéo. Notre but est de rassembler chaque compétence liée à cet univers et de travailler avec des jeunes passionnés et plus que geeks : du petit gamer dans sa chambre jusqu’à la personne souhaitant en faire son avenir.</p>
+
+        <div class="rouge"><a href="https://fr.wikipedia.org/wiki/U2">En savoir plus</a></div>
+
+    </section>
     
-    <h2>Des artistes...</h2>
+    <section>
     
-    <h2>Une zone d'échange de comptétences</h2>
+        <img src="images/sigle_geek.png" alt="Sigle Mannette" />
+        
+        <h2>Des gamers!</h2>
+        
+        <p>Oui, nous sommes des gamers en herbe, de ceux qui peuvent passer des heures bloqués devant l’épreuve du dernier jeu à la mode jusqu’à sa résolution ! <br /> <br />
 
-    <p>Nous sommes avant tout, une communauté centrée sur l’univers des jeux vidéo. Notre but est de rassembler chaque compétence liée à cet univers et de travailler avec des jeunes passionnés et plus que geeks : du petit gamer dans sa chambre jusqu’à la personne souhaitant en faire son avenir.</p>
+        Nous ne loupons aucune actualité ! Nous sommes de vrais espions du gaming. Nous ne pouvons nous passer de notre dose de potin journalier !</p>
 
-    <p>Oui, nous sommes des gamers en herbe, de ceux qui peuvent passer des heures bloqués devant l’épreuve du dernier jeu à la mode jusqu’à sa résolution ! <br /> <br />
+        <div class="bleu"><a href="https://fr.wikipedia.org/wiki/Queen">En savoir plus</a></div>
 
-    Nous ne loupons aucune actualité ! Nous sommes de vrais espions du gaming. Nous ne pouvons nous passer de notre dose de potin journalier !</p>
+    </section>
+    
+    <section>
+        
+        <img src="images/sigle_artiste.png" alt="Sigle Crayon" />
+        
+        <h2>Des artistes...</h2>
+        
+        <p>Un des points les plus importants de notre association, c’est la création ! <br /> <br />
 
-    <p>Un des points les plus importants de notre association, c’est la création ! <br /> <br />
+        Nous sommes un studio de réalisation de jeux vidéo ! Mais pas à moitié ! Le studio crée la totalité du jeu : la musique, le monde, le design, les textures... <br /> <br />
 
-    Nous sommes un studio de réalisation de jeux vidéo ! Mais pas à moitié ! Le studio crée la totalité du jeu : la musique, le monde, le design, les textures... <br /> <br />
+        C’est aussi une chaine Youtube communautaire et un réseau de chaines pour les amateurs de création vidéo gaming !</p>  
+        
+        <div class="vert"><a href="https://fr.wikipedia.org/wiki/Jean-Jacques_Goldman">En savoir plus</a></div>
 
-    C’est aussi une chaine Youtube communautaire et un réseau de chaines pour les amateurs de création vidéo gaming !</p>  
+    </section>
+    
+    <section>
+        
+        <img src="images/sigle_echange.png" alt="Sigle Echange" /> 
+        
+        <h2>Une zone d'échange de comptétences</h2>
+        
+        <p>Enfin, nous sommes une plateforme d’échange de compétences dans la création autour des jeux vidéos. <br /> <br />
 
-    <p>Enfin, nous sommes une plateforme d’échange de compétences dans la création autour des jeux vidéos. <br /> <br />
+        Afin que chacun trouve sa place, nous organisons des groupes d’échange de compétences dans la programmation, la DAO, le design... <br /> <br />
 
-    Afin que chacun trouve sa place, nous organisons des groupes d’échange de compétences dans la programmation, la DAO, le design... <br /> <br />
-
-    Ou encore, la vidéo, la musique, l’écriture d’article...etc.</p>
-
-    <div class="rouge"><a href="https://fr.wikipedia.org/wiki/U2">En savoir plus</a></div>
-
-    <div class="bleu"><a href="https://fr.wikipedia.org/wiki/Queen">En savoir plus</a></div>
-
-    <div class="vert"><a href="https://fr.wikipedia.org/wiki/Jean-Jacques_Goldman">En savoir plus</a></div>
-
-    <div class="violet"><a href="https://www.youtube.com/watch?v=vdLuk2Agamk">En savoir plus</a></div>
-
+        Ou encore, la vidéo, la musique, l’écriture d’article...etc.</p>
+        
+        <div class="violet"><a href="https://www.youtube.com/watch?v=vdLuk2Agamk">En savoir plus</a></div>
+    
+    </section>
+    
 </div>
    
 </body>
